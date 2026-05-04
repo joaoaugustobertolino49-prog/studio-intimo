85712943114d57ff0ee2b6ae07244f27fe332d6e
joaoaugustobertolino49-prog <joaoaugustobertolino49@gmail.com>
2026-05-03 23:30:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -100,5 +100,5 @@ app.get('/api/credits', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Studio rodando na porta ${PORT}`));
